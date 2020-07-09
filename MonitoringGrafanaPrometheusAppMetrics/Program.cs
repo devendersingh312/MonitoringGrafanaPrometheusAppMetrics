a75a408ca4fc57daa5e61a27ee50212136ae038f
imrakeshkh <imrakeshkh@gmail.com>
2020-07-09 17:46:11 +0530

    Initial Commit

---
+++ MonitoringGrafanaPrometheusAppMetrics/Program.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using App.Metrics;
+using App.Metrics.AspNetCore;
+using App.Metrics.Formatters.Prometheus;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace MonitoringGrafanaPrometheusAppMetrics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .UseMetricsWebTracking()
+            .UseMetrics(options =>
+            {
+                options.EndpointOptions = endpointOption =>
+                {
+                    endpointOption.MetricsTextEndpointOutputFormatter = new MetricsPrometheusTextOutputFormatter();
+                    endpointOption.MetricsEndpointOutputFormatter = new MetricsPrometheusProtobufOutputFormatter();
+                    endpointOption.EnvironmentInfoEndpointEnabled = false;
+                };
+
+            })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
